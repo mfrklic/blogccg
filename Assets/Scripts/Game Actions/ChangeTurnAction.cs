f9abe7551b0ecaff30fef2d98b13f508dc2739ae
Mickey Frklic <mfrklic@gmail.com>
2021-11-03 22:47:04 -0700

    Changing Turns

---
+++ Assets/Scripts/Game Actions/ChangeTurnAction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTurnAction : GameAction {
+    public int targetPlayerIndex;
+
+    public ChangeTurnAction (int targetPlayerIndex) {
+        this.targetPlayerIndex = targetPlayerIndex;
+    }
+}
